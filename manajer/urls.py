8c689ad6646190d6154df3efbbad67d8d91adc63
Muhammad Iqbal Dwitama <muhammad.iqbal111@ui.ac.id>
2023-05-04 16:27:12 +0700

    alter iqbal

--- manajer/urls.py
+++ manajer/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from . import views
 from .views import *
 
 app_name = 'manajer'
 
 urlpatterns = [
-    path('', views.manajerDashboard, name='manajerDashboard'),
+    path('', manajerDashboard, name='manajerDashboard'),
+    path('listStadium', listStadium, name='listStadium'),
+    path('pinjamStadium', pinjamStadium, name='pinjamStadium'),
+    path('pesanStadium', pesanStadium, name='pesanStadium'),
+    path('listPertandingan', listPertandingan, name='listPertandingan'),
+    path('historyRapat', historyRapat, name='historyRapat')
+
 ]
