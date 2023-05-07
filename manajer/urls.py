c4de0e11b319309bbe2abe33f63bb5dbee0ff6dc
Muhammad Iqbal Dwitama <muhammad.iqbal111@ui.ac.id>
2023-05-07 11:58:48 +0700

    add redirection in buttons

--- manajer/urls.py
+++ manajer/urls.py
@@ -5,11 +5,11 @@ app_name = 'manajer'
 
 urlpatterns = [
     path('', manajerDashboard, name='manajerDashboard'),
-    path('listStadium', listStadium, name='listStadium'),
-    path('pinjamStadium', pinjamStadium, name='pinjamStadium'),
-    path('pesanStadium', pesanStadium, name='pesanStadium'),
-    path('listPertandingan', listPertandingan, name='listPertandingan'),
-    path('historyRapat', historyRapat, name='historyRapat'),
+    path('listStadium/', listStadium, name='listStadium'),
+    path('pinjamStadium/', pinjamStadium, name='pinjamStadium'),
+    path('pesanStadium/', pesanStadium, name='pesanStadium'),
+    path('listPertandingan/', listPertandingan, name='listPertandingan'),
+    path('historyRapat/', historyRapat, name='historyRapat'),
     path('kelolatim/', kelolaTim, name='kelolaTim'),
     path('daftartim/', daftarTim, name='daftarTim'),
     path('daftarpemain/', daftarPemain, name='daftarPemain'),
