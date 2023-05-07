436967667235c19f57d9cd52ef09e6feab67c2a6
ameliaptrc <ameliaptrch@gmail.com>
2023-05-07 13:08:25 +0700

    2x

--- auth/urls.py
+++ auth/urls.py
@@ -4,7 +4,6 @@ from auth.views import show_login, show_register, show_home
 app_name = 'auth'
 
 urlpatterns = [
-    path('', show_home, name='show_home'),
     path('register/', show_register, name='show_register' ),
     path('login/', show_login, name='show_login' ),
 ]
--- auth/views.py
+++ auth/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
-def show_home(request):
-    return render(request, "home.html")
 
 def show_login(request):
     return render(request, "login.html")
