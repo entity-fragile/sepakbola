143988e66b5f6ef541d6751dd70cefd8a2e529a2
Muhammad Iqbal Dwitama <muhammad.iqbal111@ui.ac.id>
2023-05-15 16:44:16 +0700

    fix auth

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Kuliah\Semester 4\Basdat\TK\badudu\env
+set VIRTUAL_ENV=D:\Kuliah\Semester 4\Basdat\TK\baru\basdat\env
 
 if not defined PROMPT set PROMPT=$P$G
 
