5e69e726ff484ee3d4b0ac41b009425eab80827b
Muhammad Rifqi Adli Gumay <88142116+asta-kunn@users.noreply.github.com>
2023-05-21 13:32:56 +0700

    update setting

--- babadu/settings.py
+++ babadu/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = 'django-insecure-@$a7vmh!m0kcnohstmsp1b^@=&22=f^96!c$b4dnwcbrg=krdg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://sepakbola-production.up.railway.app/', 'localhost', '127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['https://sepakbola-production.up.railway.app/']
+ALLOWED_HOSTS = ['sepakbola-production.up.railway.app', 'localhost', '127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['sepakbola-production.up.railway.app']
 
 
 # Application definition
